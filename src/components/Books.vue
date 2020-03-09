5c2135d87796450106b9971d321524764a7cd276
aaleksandrov <aladex@gmail.com>
2020-03-09 11:41:57 +0300

    поиск книг вынес в отдельный компонент

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -2,36 +2,7 @@
     <v-container
             fluid
     >
-        <v-row justify="center">
-            <v-col
-                    lg="8"
-                    sm="10"
-            >
-                <v-row justify="start">
-                    <v-col lg="4"
-                           sm="8">
-                        <v-text-field
-                                v-model="localTitle"
-                                label="Искать книгу по названию"
-                                single-line
-                                hide-details
-                                @keyup.enter="findByTitle"
-                        >
-
-                        </v-text-field>
-                    </v-col>
-                    <v-col lg="2" sm="4">
-                        <v-btn
-                                class="search-btn"
-                                @click="findByTitle"
-                        >
-                            Искать
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-col>
-        </v-row>
-
+        <book-find></book-find>
         <v-row
                 v-for="b in books"
                 justify="center"
@@ -114,9 +85,13 @@
 </template>
 
 <script>
+    import BookFind from "@/components/utils/BookFind";
     export default {
         name: "Books",
         props: ["page", "title"],
+        components: {
+            BookFind
+        },
         data() {
             return {
                 pagesLength: 1,
@@ -152,9 +127,6 @@
                 } else {
                     this.$router.push(`/page/${page}`)
                 }
-            },
-            findByTitle() {
-                this.$router.push(`/find/books/${this.localTitle}/1`)
             },
 
             toHumanDate(value) {
---
+++ src/components/utils/BookFind.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-row justify="center">
+        <v-col
+                lg="8"
+                sm="10"
+        >
+            <v-row justify="start">
+                <v-col lg="4"
+                       sm="8">
+                    <v-text-field
+                            v-model="localTitle"
+                            label="Искать книгу по названию"
+                            single-line
+                            hide-details
+                            @keyup.enter="findByTitle"
+                    >
+
+                    </v-text-field>
+                </v-col>
+                <v-col lg="2" sm="4">
+                    <v-btn
+                            class="search-btn"
+                            @click="findByTitle"
+                    >
+                        Искать
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: "BookFind",
+        methods: {
+            findByTitle() {
+                this.$router.push(`/find/books/${this.localTitle}/1`)
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
