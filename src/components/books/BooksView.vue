1ab9a47ef6534f4cc34133fed4e3bc60daa5892b
Andrey Aleksandrov <aladex@gmail.com>
2021-09-14 01:04:12 +0200

    moved posters to cdn

--- src/components/books/BooksView.vue
+++ src/components/books/BooksView.vue
@@ -356,9 +356,9 @@
                 if (b.cover) {
                     let path = b.path.replace(".", '-');
                     let img = b.filename.replace(".", '-');
-                    return `${process.env.VUE_APP_BACKEND_API_URL}books-posters/${path}/${img}.jpg`
+                    return `${process.env.VUE_APP_CDN_URL}books-posters/${path}/${img}.jpg`
                 }
-                return "${process.env.VUE_APP_BACKEND_API_URL}books-posters/no-cover.png"
+                return "${process.env.VUE_APP_CDN_URL}books-posters/no-cover.png"
             },
             makeShort(b) {
                 if (b.annotation === undefined) {
